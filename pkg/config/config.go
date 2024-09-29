lindonm/vikunja

commit 47c5578863e07f8984b4371f30513d3016d57585

feat: auto tls
This commit introduces the automatic retrieval of TLS certificates from Let's Encrypt. If the feature is enabled, Vikunja will automagically request a certificate from Let's Encrypt and configure it to server content via TLS.

pkg/config/config.go
@@ -183,6 +183,10 @@ const (
 	WebhooksTimeoutSeconds Key = `webhooks.timeoutseconds`
 	WebhooksProxyURL       Key = `webhooks.proxyurl`
 	WebhooksProxyPassword  Key = `webhooks.proxypassword`
+
+	AutoTLSEnabled     Key = `autotls.enabled`
+	AutoTLSEmail       Key = `autotls.email`
+	AutoTLSRenewBefore Key = `autotls.renewbefore`
 )
 
 // GetString returns a string config value
@@ -407,6 +411,8 @@ func InitDefaultConfig() {
 	// Webhook
 	WebhooksEnabled.setDefault(true)
 	WebhooksTimeoutSeconds.setDefault(30)
+	// AutoTLS
+	AutoTLSRenewBefore.setDefault("720h") // 30days in hours
 }
 
 // InitConfig initializes the config, sets defaults etc.
@@ -480,6 +486,13 @@ func InitConfig() {
 	if ServiceEnableMetrics.GetBool() {
 		log.Warning("service.enablemetrics is deprecated and will be removed in a future release. Please use metrics.enable.")
 		MetricsEnabled.Set(true)
+	}
+
+	if !strings.HasPrefix(FilesBasePath.GetString(), "/") {
+		FilesBasePath.Set(filepath.Join(
+			ServiceRootpath.GetString(),
+			FilesBasePath.GetString(),
+		))
 	}
 }
 

pkg/cmd/web.go
@@ -19,8 +19,11 @@ package cmd
 import (
 	"context"
 	"net"
+	"net/url"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"code.vikunja.io/api/pkg/config"
@@ -33,6 +36,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func init() {
@@ -64,6 +68,52 @@ func setupUnixSocket(e *echo.Echo) error {
 	return nil
 }
 
+func setupAutoTLS(e *echo.Echo) {
+	if config.ServiceUnixSocket.GetString() != "" {
+		log.Warning("Auto tls is enabled but listening on a unix socket is enabled as well. The latter will be ignored.")
+	}
+	if config.ServicePublicURL.GetString() == "" {
+		log.Fatal("You must configure a publicurl to use autotls.")
+	}
+	parsed, err := url.Parse(config.ServicePublicURL.GetString())
+	if err != nil {
+		log.Fatalf("Could not parse hostname from publicurl: %s", err)
+	}
+	domain := parsed.Hostname()
+	if domain == "" {
+		log.Fatalf("The hostname cannot be empty. Please make sure the configured publicurl contains a hostname.")
+	}
+	if !strings.Contains(domain, ".") {
+		log.Fatalf("The hostname must be a valid TLD. Please make sure the configured publicurl contains a valid TLD.")
+	}
+	renew, err := time.ParseDuration(config.AutoTLSRenewBefore.GetString())
+	if err != nil {
+		log.Fatalf("autotls.renewbefore must be a valid duration: %s", err)
+	}
+	if config.AutoTLSEmail.GetString() == "" {
+		log.Fatalf("You must provide an email address to use autotls.")
+	}
+	e.AutoTLSManager = autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		Cache: autocert.DirCache(filepath.Join(
+			config.FilesBasePath.GetString(),
+			".certs",
+		)),
+		HostPolicy:  autocert.HostWhitelist(domain),
+		RenewBefore: renew,
+		Email:       config.AutoTLSEmail.GetString(),
+	}
+
+	if config.ServiceInterface.GetString() != ":443" {
+		log.Warningf("Vikunja's interface is set to %s, with tls it is recommended to set this to :443", config.ServiceInterface.GetString())
+	}
+
+	err = e.StartAutoTLS(config.ServiceInterface.GetString())
+	if err != nil {
+		e.Logger.Info("shutting down...")
+	}
+}
+
 var webCmd = &cobra.Command{
 	Use:   "web",
 	Short: "Starts the rest api web server",
@@ -80,18 +130,26 @@ var webCmd = &cobra.Command{
 		routes.RegisterRoutes(e)
 		// Start server
 		go func() {
+			if config.AutoTLSEnabled.GetBool() {
+				setupAutoTLS(e)
+				return
+			}
+
 			// Listen unix socket if needed (ServiceInterface will be ignored)
 			if config.ServiceUnixSocket.GetString() != "" {
 				if err := setupUnixSocket(e); err != nil {
 					e.Logger.Fatal(err)
 				}
+				return
 			}
-			if err := e.Start(config.ServiceInterface.GetString()); err != nil {
+
+			err := e.Start(config.ServiceInterface.GetString())
+			if err != nil {
 				e.Logger.Info("shutting down...")
 			}
 		}()
 
-		// Wait for interrupt signal to gracefully shutdown the server with
+		// Wait for interrupt signal to gracefully shut down the server with
 		// a timeout of 10 seconds.
 		quit := make(chan os.Signal, 1)
 		signal.Notify(quit, os.Interrupt)

commit 820f57e10df9f659ff7799d0f8e686ad0a2e5a4d

fix(files): configure the files path in files init instead of globally
This fixes a regression introduced in 47c5578863e07f8984b4371f30513d3016d57585 where the root path would be included twice in the file path, leading to retrieval issues.

pkg/config/config.go
@@ -487,13 +487,6 @@ func InitConfig() {
 		log.Warning("service.enablemetrics is deprecated and will be removed in a future release. Please use metrics.enable.")
 		MetricsEnabled.Set(true)
 	}
-
-	if !strings.HasPrefix(FilesBasePath.GetString(), "/") {
-		FilesBasePath.Set(filepath.Join(
-			ServiceRootpath.GetString(),
-			FilesBasePath.GetString(),
-		))
-	}
 }
 
 func random(length int) (string, error) {

pkg/files/filehandling.go
@@ -18,8 +18,11 @@ package files
 
 import (
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
+	"code.vikunja.io/api/pkg/config"
 	"code.vikunja.io/api/pkg/db"
 	"code.vikunja.io/api/pkg/log"
 	"code.vikunja.io/api/pkg/modules/keyvalue"
@@ -32,16 +35,27 @@ import (
 var fs afero.Fs
 var afs *afero.Afero
 
+func setDefaultConfig() {
+	if !strings.HasPrefix(config.FilesBasePath.GetString(), "/") {
+		config.FilesBasePath.Set(filepath.Join(
+			config.ServiceRootpath.GetString(),
+			config.FilesBasePath.GetString(),
+		))
+	}
+}
+
 // InitFileHandler creates a new file handler for the file backend we want to use
 func InitFileHandler() {
 	fs = afero.NewOsFs()
 	afs = &afero.Afero{Fs: fs}
+	setDefaultConfig()
 }
 
 // InitTestFileHandler initializes a new memory file system for testing
 func InitTestFileHandler() {
 	fs = afero.NewMemMapFs()
 	afs = &afero.Afero{Fs: fs}
+	setDefaultConfig()
 }
 
 func initFixtures(t *testing.T) {

pkg/files/files.go
@@ -22,7 +22,6 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
-	"strings"
 	"time"
 
 	"code.vikunja.io/api/pkg/config"
@@ -59,12 +58,7 @@ func (*File) TableName() string {
 }
 
 func (f *File) getAbsoluteFilePath() string {
-	base := ""
-	if !strings.HasPrefix(config.FilesBasePath.GetString(), "/") {
-		base = config.ServiceRootpath.GetString()
-	}
 	return filepath.Join(
-		base,
 		config.FilesBasePath.GetString(),
 		strconv.FormatInt(f.ID, 10),
 	)

pkg/models/task_attachment_test.go
@@ -51,7 +51,7 @@ func TestTaskAttachment_ReadOne(t *testing.T) {
 		// Load the actual attachment file and check its content
 		err = ta.File.LoadFileByID()
 		require.NoError(t, err)
-		assert.Equal(t, filepath.Join(config.ServiceRootpath.GetString(), config.FilesBasePath.GetString(), "/1"), ta.File.File.Name())
+		assert.Equal(t, filepath.Join(config.ServiceRootpath.GetString(), "files", "1"), ta.File.File.Name())
 		content := make([]byte, 9)
 		read, err := ta.File.File.Read(content)
 		require.NoError(t, err)