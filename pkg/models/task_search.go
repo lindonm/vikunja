lindonm/vikunja

commit f20f01d62636b953afa53f40a36c38e3a84ee856

feat(filters): allow filtering by created and updated task fields
Resolves https://kolaente.dev/vikunja/vikunja/issues/2768

pkg/models/tasks.go
@@ -212,7 +212,7 @@ func (t *Task) ReadAll(_ *xorm.Session, _ web.Auth, _ string, _ int, _ int) (res
 }
 
 func getFilterCond(f *taskFilter, includeNulls bool) (cond builder.Cond, err error) {
-	field := "`" + f.field + "`"
+	field := "tasks.`" + f.field + "`"
 	if f.field == taskPropertyBucketID {
 		field = "task_buckets.`bucket_id`"
 	}

frontend/src/components/project/partials/FilterInputDocs.vue
@@ -31,6 +31,8 @@ const showDocs = ref(false)
 			<li><code>labels</code>: {{ $t('filters.query.help.fields.labels') }}</li>
 			<li><code>project</code>: {{ $t('filters.query.help.fields.project') }}</li>
 			<li><code>reminders</code>: {{ $t('filters.query.help.fields.reminders') }}</li>
+			<li><code>created</code>: {{ $t('filters.query.help.fields.created') }}</li>
+			<li><code>updated</code>: {{ $t('filters.query.help.fields.updated') }}</li>
 		</ul>
 		<p>{{ $t('filters.query.help.canUseDatemath') }}</p>
 		<p>{{ $t('filters.query.help.operators.intro') }}</p>

frontend/src/helpers/filters.ts
@@ -6,6 +6,8 @@ export const DATE_FIELDS = [
 	'endDate',
 	'doneAt',
 	'reminders',
+	'created',
+	'updated',
 ]
 
 export const ASSIGNEE_FIELDS = [

commit cbda0196f186f2799508a74b46ddacb71cc660fa

fix: do not prefix tasks all the time
Fixes a regression from f20f01d62636b953afa53f40a36c38e3a84ee856

pkg/models/task_search.go
@@ -179,6 +179,11 @@ func convertFiltersToDBFilterCond(rawFilters []*taskFilter, includeNulls bool) (
 			continue
 		}
 
+		if f.field == taskPropertyBucketID {
+			f.field = "task_buckets.`bucket_id`"
+		} else {
+			f.field = "tasks.`" + f.field + "`"
+		}
 		filter, err := getFilterCond(f, includeNulls)
 		if err != nil {
 			return nil, err

pkg/models/tasks.go
@@ -212,10 +212,8 @@ func (t *Task) ReadAll(_ *xorm.Session, _ web.Auth, _ string, _ int, _ int) (res
 }
 
 func getFilterCond(f *taskFilter, includeNulls bool) (cond builder.Cond, err error) {
-	field := "tasks.`" + f.field + "`"
-	if f.field == taskPropertyBucketID {
-		field = "task_buckets.`bucket_id`"
-	}
+	field := f.field
+
 	switch f.comparator {
 	case taskFilterComparatorEquals:
 		cond = &builder.Eq{field: f.value}