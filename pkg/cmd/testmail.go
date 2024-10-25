lindonm/vikunja

commit 950de7c954ef3799051d73bc9b728558cf166377

fix(mail): do not fail testmail command when the connection could not be closed.
Resolves https://github.com/go-vikunja/vikunja/issues/300

(cherry picked from commit 40bb86bee59585f827752e96c87e5e8c8f81495c)

pkg/cmd/testmail.go
@@ -17,6 +17,8 @@
 package cmd
 
 import (
+	"strings"
+
 	"code.vikunja.io/api/pkg/config"
 	"code.vikunja.io/api/pkg/initialize"
 	"code.vikunja.io/api/pkg/log"
@@ -51,10 +53,11 @@ var testmailCmd = &cobra.Command{
 
 		opts, err := notifications.RenderMail(message)
 		if err != nil {
-			log.Errorf("Error sending test mail: %s", err.Error())
+			log.Errorf("Error rendering test mail: %s", err.Error())
 			return
 		}
-		if err := mail.SendTestMail(opts); err != nil {
+		if err := mail.SendTestMail(opts); err != nil &&
+			strings.HasPrefix(err.Error(), "failed to close connction: not connected to SMTP server") {
 			log.Errorf("Error sending test mail: %s", err.Error())
 			return
 		}

pkg/mail/send_mail.go
@@ -18,6 +18,7 @@ package mail
 
 import (
 	"embed"
+	"fmt"
 	"io"
 
 	"code.vikunja.io/api/pkg/config"
@@ -60,8 +61,7 @@ type header struct {
 // It works without a queue.
 func SendTestMail(opts *Opts) error {
 	if config.MailerHost.GetString() == "" {
-		log.Warning("Mailer seems to be not configured! Please see the config docs for more details.")
-		return nil
+		return fmt.Errorf("mailer is not configured! Please see the config docs for more details")
 	}
 
 	c, err := getClient()

commit 075e517ab26e945310a2432d77d5180be36bc470

fix(cmd): report error when the connection to the mail server failed
When running the testmail command, Vikunja would not stop if it wasn't able to connect to the mail server. This was a regression from 950de7c954ef3799051d73bc9b728558cf166377.
This change fixes that problem.

Resolves https://kolaente.dev/vikunja/vikunja/issues/2767

pkg/cmd/testmail.go
@@ -57,7 +57,7 @@ var testmailCmd = &cobra.Command{
 			return
 		}
 		if err := mail.SendTestMail(opts); err != nil &&
-			strings.HasPrefix(err.Error(), "failed to close connction: not connected to SMTP server") {
+			!strings.HasPrefix(err.Error(), "failed to close connction: not connected to SMTP server") {
 			log.Errorf("Error sending test mail: %s", err.Error())
 			return
 		}