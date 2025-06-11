lindonm/vikunja

commit 5ed5ef0a4ad1f52cc2dd1c094058b8cf376debc5

feat: enable cors by default for desktop app (#904)

pkg/config/config.go
@@ -421,8 +421,8 @@ func InitDefaultConfig() {
 	FilesBasePath.setDefault("files")
 	FilesMaxSize.setDefault("20MB")
 	// Cors
-	CorsEnable.setDefault(false)
-	CorsOrigins.setDefault([]string{"*"})
+	CorsEnable.setDefault(true)
+	CorsOrigins.setDefault([]string{"http://127.0.0.1:*", "http://localhost:*"})
 	CorsMaxAge.setDefault(0)
 	// Migration
 	MigrationTodoistEnable.setDefault(false)

commit df35fc170228fda24968c222ab73a299a70f9409

fix: always add public url to allowed cors origins
This fixes a bug where it was not possible to do anything because the public url was not allowed by default for CORS requests.

Regression from 5ed5ef0a4ad1f52cc2dd1c094058b8cf376debc5

Resolves https://github.com/go-vikunja/vikunja/issues/916

pkg/config/config.go
@@ -599,6 +599,9 @@ func InitConfig() {
 	if DefaultSettingsTimezone.GetString() == "" {
 		DefaultSettingsTimezone.Set(ServiceTimeZone.GetString())
 	}
+
+	publicURL := strings.TrimSuffix(ServicePublicURL.GetString(), "/")
+	CorsOrigins.Set(append(CorsOrigins.GetStringSlice(), publicURL))
 }
 
 func random(length int) (string, error) {

pkg/routes/routes.go
@@ -182,6 +182,7 @@ func RegisterRoutes(e *echo.Echo) {
 
 	// CORS
 	if config.CorsEnable.GetBool() {
+		log.Debugf("CORS enabled with origins: %s", strings.Join(config.CorsOrigins.GetStringSlice(), ", "))
 		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 			AllowOrigins: config.CorsOrigins.GetStringSlice(),
 			MaxAge:       config.CorsMaxAge.GetInt(),