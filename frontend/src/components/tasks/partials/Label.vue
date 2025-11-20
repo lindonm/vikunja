lindonm/vikunja

commit 12b133b0cc199c61fb5e880935acc3a9f7182345

fix(table): label spacing when wrapping

frontend/src/components/tasks/partials/Label.vue
@@ -21,8 +21,5 @@ const {getLabelStyles} = useLabelStyles()
 
 <style scoped lang="scss">
 .tag {
-	& + & {
-		margin-inline-start: 0.5rem;
-	}
 }
 </style>

frontend/src/components/tasks/partials/Labels.vue
@@ -26,11 +26,13 @@ const displayLabels = computed(() =>
 
 <style lang="scss" scoped>
 .label-wrapper {
-	display: inline;
-	
+	display: inline-flex;
+	flex-wrap: wrap;
+	gap: 0.25rem;
+	align-items: flex-start;
+
 	:deep(.tag) {
-		margin-block-start: .125rem;
-		margin-block-end: .125rem;
+		margin: 0;
 	}
 }
 </style>

commit c145bf3577109df9cd802f02c6d350cf0d0f05cf

fix: remove empty style block from Label.vue
Fixes a regression introduced in 12b133b0cc199c61fb5e880935acc3a9f7182345

frontend/src/components/tasks/partials/Label.vue
@@ -18,8 +18,3 @@ const {getLabelStyles} = useLabelStyles()
 		<span>{{ label.title }}</span>
 	</span>
 </template>
-
-<style scoped lang="scss">
-.tag {
-}
-</style>